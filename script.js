aadcaebe250aa3b531093c7fe2a3481b01c2a570
hcl-ericsson <krishnaanand42@gmail.com>
2026-08-15 12:39:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ let xScore = 0;
 let oScore = 0;
 
 const winningCombos = [
-  [0,1,2],[3,4,5],[6,7,8],
-  [0,3,6],[1,4,7],[2,5,8],
-  [0,4,8],[2,4,6]
+  [0,1,2], [3,4,5], [6,7,8],
+  [0,3,6], [1,4,7], [2,5,8],
+  [0,4,8], [2,4,6]
 ];
 
 cells.forEach(cell => cell.addEventListener('click', handleClick));
@@ -38,9 +38,13 @@ function handleClick(e) {
 
 function checkWinner() {
   for (let combo of winningCombos) {
-    const [a,b,c] = combo;
+    const [a, b, c] = combo;
 
-    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+    if (
+      board[a] &&
+      board[a] === board[b] &&
+      board[a] === board[c]
+    ) {
       gameActive = false;
 
       cells[a].classList.add('win');
